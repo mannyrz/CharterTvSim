6f86832e709610f64c12a87ebd219028e852ec72
MRodriguez <mrodriguez@MRodriguezs-MBP.hsd1.co.comcast.net>
2020-05-11 07:59:53 -0600

    Add Read Master Schedule

--- MasterTVProgram/Master.py
+++ MasterTVProgram/Master.py
@@ -1,10 +1,23 @@
+import os
+import json
+
 class Master:
-	pass
+	
+	def __init__(self, userName, channelList=None):
+		self.userName = userName
+		self.channeList = channelList
 
 	def getCurrentTime(self):
 		pass
 
-	def getCurrentChannels(self):
-		pass
+	def CurrentSchedule(self, masterScheduleList):
+		file_path = 'resources/master/' + masterScheduleList
+		
+		with open(file_path) as Resouce_File:
+			 master_schedule =  json.load(Resouce_File)
+			 return master_schedule
 
-print("Test")
+print("Testing")
+
+ts = Master.CurrentSchedule('Test', 'currentSchedule.json')
+print(ts)
